5030432993159dbf1d13505cd5270274ef2ca24c
Meher Malladi <rm.meher97@gmail.com>
2023-04-18 22:08:54 +0200

    please

--- .config/tools/install.sh
+++ .config/tools/install.sh
@@ -1,17 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # the entry install script
 install_package() 
 {
-        sudo apt install --no-install-recommends -y "$1"
+        sudo apt install --no-install-recommends -y "$@"
 }
 
-install_package git
-install_package neovim
-install_package zsh
-install_package bat
-install_package kitty
-install_package build-essential
-install_package ripgrep
+install_package git neovim zsh bat kitty build-essential ripgrep curl zsh
 
 # some initial setup
 LOCAL_SRC=$HOME/.local/src
@@ -25,6 +19,5 @@ git clone https://github.com/TheLocehiliosan/yadm.git "$LOCAL_SRC"/yadm
 ln -s "$LOCAL_SRC"/yadm/yadm "$LOCAL_BIN"/yadm 
 
 # TODO: switch this to ssh git later
-yadm clone -f --no-bootstrap https://github.com/mehermvr/dotfiles.git
-# just to make sure our bootstrap logic is always independent
+yadm clone -f --bootstrap https://github.com/mehermvr/dotfiles.git
 yadm bootstrap
