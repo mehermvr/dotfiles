26b711b60e730b159551db5f59e65dad00dca0cd
Meher Malladi <meher.malladi@uni-bonn.de>
2023-05-18 18:05:11 +0200

    lsp and autocompletion remains

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,44 +19,48 @@ vim.g.mapleader = ' '
 local lazy = {}
 
 function lazy.install(path)
-  if not vim.loop.fs_stat(path) then
-    print('Installing lazy.nvim....')
-    vim.fn.system({
-      'git',
-      'clone',
-      '--filter=blob:none',
-      'https://github.com/folke/lazy.nvim.git',
-      '--branch=stable', -- latest stable release
-      path,
-    })
-  end
+	if not vim.loop.fs_stat(path) then
+		print('Installing lazy.nvim....')
+		vim.fn.system({
+			'git',
+			'clone',
+			'--filter=blob:none',
+			'https://github.com/folke/lazy.nvim.git',
+			'--branch=stable', -- latest stable release
+			path,
+		})
+	end
 end
 
 function lazy.setup(plugins)
-  lazy.install(lazy.path)
-  vim.opt.rtp:prepend(lazy.path)
-  require('lazy').setup(plugins, lazy.opts)
+	lazy.install(lazy.path)
+	vim.opt.rtp:prepend(lazy.path)
+	require('lazy').setup(plugins, lazy.opts)
 end
 
 lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 lazy.opts = {}
 
 lazy.setup({
-	{ "ellisonleao/gruvbox.nvim", lazy = false, priority = 1000 },
-	{"nvim-lualine/lualine.nvim"},
-	{"nvim-tree/nvim-web-devicons"},
-	{'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
-	{ "lukas-reineke/indent-blankline.nvim" },
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-	{"nvim-treesitter/nvim-treesitter-textobjects",dependencies= "nvim-treesitter/nvim-treesitter",
+	{'ellisonleao/gruvbox.nvim', lazy = false, priority = 1000 },
+	{'nvim-lualine/lualine.nvim'},
+	{'nvim-tree/nvim-web-devicons'},
+	{'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons'},
+	{'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+	{'nvim-treesitter/nvim-treesitter-textobjects', dependencies= 'nvim-treesitter/nvim-treesitter'},
 	{'wellle/targets.vim'},
 	{'numToStr/Comment.nvim'},
-}
-
+	{'lukas-reineke/indent-blankline.nvim'},
+	{'tpope/vim-surround'},
+	{'kyazdani42/nvim-tree.lua'},
+	{'nvim-telescope/telescope.nvim', dependencies = 'nvim-lua/plenary.nvim'},
+	{'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'},
+	{'lewis6991/gitsigns.nvim'},
+	{ 'tpope/vim-repeat' },
 })
 
 vim.opt.termguicolors = true
-vim.o.background = "dark" -- or "light" for light mode
+vim.o.background = 'dark' -- or "light" for light mode
 vim.cmd.colorscheme('gruvbox')
 vim.opt.showmode = false
 -- most of this follows https://vonheikemen.github.io/devlog/tools/neovim-plugins-to-get-started/, need to read the docs myself
@@ -70,80 +74,88 @@ require('lualine').setup({
 	}
 })
 require('bufferline').setup({
-  options = {
-    mode = 'buffers',
-    offsets = {
-      {filetype = 'NvimTree'}
-    },
-  },
-  highlights = {
-    indicator_selected = {
-      fg = {attribute = 'fg', highlight = 'Function'},
-    }
-  }
+	options = {
+		mode = 'buffers',
+		offsets = {
+			{filetype = 'NvimTree'}
+		},
+	},
+	highlights = {
+		indicator_selected = {
+			fg = {attribute = 'fg', highlight = 'Function'},
+		}
+	}
 })
 
 require('nvim-treesitter.configs').setup({
-  highlight = {
-    enable = true,
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      }
-    },
-  },
-  ensure_installed = {
-	'bash',
-	'bibtex',
-	'c',
-	'cmake',
-	'cpp',
-	'css',
-	'cuda',
-	'dockerfile',
-	'git_config',
-	'git_rebase',
-	'gitattributes',
-	'gitcommit',
-	'gitignore',
-	'glsl',
-	'html',
-	'ini',
-	'json',
-    'json5',
-	'latex',
-	'lua',
-	'luadoc',
-	'make',
-	'markdown',
-	'markdown_inline',
-	'meson',
-	'ninja',
-	'python',
-	'rasi',
-	'regex',
-	'rst',
-	'rust',
-	'toml',
-	'vim',
-	'vimdoc',
-	'yaml',
-  },
+	highlight = {
+		enable = true,
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				['af'] = '@function.outer',
+				['if'] = '@function.inner',
+				['ac'] = '@class.outer',
+				['ic'] = '@class.inner',
+			}
+		},
+	},
+	ensure_installed = {
+		'bash',
+		'bibtex',
+		'c',
+		'cmake',
+		'cpp',
+		'css',
+		'cuda',
+		'dockerfile',
+		'git_config',
+		'git_rebase',
+		'gitattributes',
+		'gitcommit',
+		'gitignore',
+		'glsl',
+		'html',
+		'ini',
+		'json',
+		'json5',
+		'latex',
+		'lua',
+		'luadoc',
+		'make',
+		'markdown',
+		'markdown_inline',
+		'meson',
+		'ninja',
+		'python',
+		'rasi',
+		'regex',
+		'rst',
+		'rust',
+		'toml',
+		'vim',
+		'vimdoc',
+		'yaml',
+	},
 })
-
 require('indent_blankline').setup({
-	char = '▏',
 	show_trailing_blankline_indent = false,
 	show_first_indent_level = false,
-	use_treesitter = true,
-	show_current_context = false
+	-- use_treesitter = true,
+	show_current_context = true,
+	space_char_blankline = " ",
+	char_highlight_list = {
+		"IndentBlanklineIndent1",
+	}
 })
 
 require('Comment').setup({})
+require('nvim-tree').setup({
+})
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>')
+
+require('telescope').load_extension('fzf')
+require('gitsigns').setup()
