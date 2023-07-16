2a1378c0b00beec8835f2de483425f1b540f6313
Meher Malladi <rm.meher97@gmail.com>
2023-07-16 14:54:37 +0200

    changes

--- .config/tools/setup-ssh-agent.sh
+++ .config/tools/setup-ssh-agent.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 systemctl --user enable ssh-agent.service
 systemctl --user start ssh-agent.service
-echo "AddKeysToAgent yes" >> .ssh/config
+echo "AddKeysToAgent yes" >> $HOME/.ssh/config
