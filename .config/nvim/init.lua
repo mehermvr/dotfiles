e890838d0c8cd5311903592c3e2c3ad202d48b5a
Meher Malladi <rm.meher97@gmail.com>
2024-01-01 16:31:35 +0530

    ok, i give up on nvim config

--- .config/nvim/init.lua
+++
@@ -1,4 +0,0 @@
--- this follows much of the style of LazyVim/LazyVim by folke.
--- but since I don't need to support arbitrary users and arbitrary plugin specs, but i do want to make specific changes that basically need a ground up write anyways, just doing it
--- bootstrap lazy.nvim and plugins
-require("start")
--- .config/nvim/lua/themes/core.lua
+++
@@ -1,6 +0,0 @@
-local M = {}
-
--- TODO: theme switcher
-M.kanagawa = require("themes.kanagawa")
-
-return { M.kanagawa }
