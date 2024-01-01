eed86adacb302f9d24235df91319b33cd8aa9f3d
Meher Malladi <rm.meher97@gmail.com>
2024-01-01 10:52:00 +0530

    kanagawa cleanup

--- .config/nvim/lua/themes/kanagawa.lua
+++ .config/nvim/lua/themes/kanagawa.lua
@@ -4,28 +4,9 @@ local kanagawa = {
   lazy = false,
   config = function(_, opts)
     require("kanagawa").setup(opts)
-    -- vim.cmd([[colorscheme kanagawa]])
     require("kanagawa").load()
   end,
 
-  -- overrides = function(colors)
-  --   local theme = colors.theme
-  --   return {
-  --     NormalFloat = { bg = "none" },
-  --     FloatBorder = { bg = "none" },
-  --     FloatTitle = { bg = "none" },
-  --
-  --     -- Save an hlgroup with dark background and dimmed foreground
-  --     -- so that you can use it where your still want darker windows.
-  --     -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-  --     NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-  --
-  --     -- Popular plugins that open floats will link to NormalFloat by default;
-  --     -- set their background accordingly if you wish to keep them dark and borderless
-  --     LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-  --     MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-  --   }
-  -- end,
   opts = {
     overrides = function(colors)
       local theme = colors.theme
