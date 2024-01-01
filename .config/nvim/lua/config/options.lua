eeb0296203975c4f4378753bcab677d0d7e41334
Meher Malladi <rm.meher97@gmail.com>
2024-01-01 17:59:45 +0530

    lazyvim managed nvim config

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,5 @@
+-- Options are automatically loaded before lazy.nvim startup
+local opt = vim.opt
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+opt.conceallevel = 0 -- 3 - Hide * markup for bold and italic
---
+++ .config/nvim/lua/plugins/languages.lua
@@ -0,0 +1,21 @@
+return {
+  -- python
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.python-semshi" },
+  { import = "lazyvim.plugins.extras.formatting.black" },
+  -- clangd
+  { import = "lazyvim.plugins.extras.lang.clangd" },
+  -- cmake
+  { import = "lazyvim.plugins.extras.lang.cmake" },
+  -- markdown
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  -- docker
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  -- json
+  { import = "lazyvim.plugins.extras.lang.json" },
+  -- tex
+  { import = "lazyvim.plugins.extras.lang.tex" },
+  -- yaml
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+}
