0fa7df4ea3128b564a43227ae501528678261267
Meher Malladi <meher.malladi@uni-bonn.de>
2024-08-20 11:08:21 +0200

    python nvim config cleanup

--- .config/nvim/lua/plugins/languages.lua
+++ .config/nvim/lua/plugins/languages.lua
@@ -1,8 +1,9 @@
 return {
+  { import = "lazyvim.plugins.extras.lang.git" },
   -- python
   { import = "lazyvim.plugins.extras.lang.python" },
-  -- { import = "lazyvim.plugins.extras.lang.python-semshi" },
-  -- { import = "lazyvim.plugins.extras.formatting.black" },
+  { import = "lazyvim.plugins.extras.formatting.black" },
+  { import = "lazyvim.plugins.extras.lang.toml" },
   -- clangd
   { import = "lazyvim.plugins.extras.lang.clangd" },
   -- cmake
